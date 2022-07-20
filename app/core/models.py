1df65e09ee2199edf5fc9213c55ee697a5938596
Consdev <amirthedevops@gmail.com>
2022-07-20 14:17:22 +0430

    - Added Topic, Feed and Comment models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=30)
+    followed_by = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Feed(models.Model):
+    title = models.TextField(unique=True)
+    link = models.CharField(max_length=255)
+    description = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    read_by = models.ManyToManyField(
+                                    settings.AUTH_USER_MODEL,
+                                    related_name="read_feeds"
+                                    )
+    favourite_by = models.ManyToManyField(
+                                    settings.AUTH_USER_MODEL,
+                                    related_name="favorite_feeds"
+                                    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    commenter = models.ForeignKey(
+                                settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE
+                                )
+    commented_on = models.ForeignKey(Feed, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.content} by {self.commenter.username}'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,40 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def create_topic(**params):
+    defaults = {
+        'name': 'Test topic',
+        'link': 'Test link'
+    }
+
+    defaults.update(params)
+    return models.Topic.objects.create(**defaults)
+
+
+def create_user(**params):
+    defaults = {
+        'username': 'testusername',
+        'password': 'testpass'
+    }
+
+    defaults.update(params)
+    return get_user_model().objects.create_user(**defaults)
+
+
+def create_feed(**params):
+    defaults = {
+        'title': "Weather",
+        'link': "test",
+        'description': 'Tomorrow forecast',
+        'topic': create_topic()
+    }
+
+    defaults.update(params)
+    return models.Feed.objects.create(**defaults)
+
 
 class ModelTests(TestCase):
 
@@ -29,3 +63,30 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+    def test_feed_str_repr(self):
+        '''Test the string representation of Feed instances'''
+        feed = create_feed()
+
+        self.assertEqual(str(feed), feed.title)
+
+    def test_comment_str_repr(self):
+        '''Test the string representation of Comment instances'''
+        comment = models.Comment.objects.create(
+            content='Good stuff',
+            commenter=create_user(),
+            commented_on=create_feed()
+            )
+
+        self.assertEqual(
+            str(comment), f'{comment.content} by {comment.commenter.username}'
+            )
+
+    def test_topic_str_repr(self):
+        '''Test the string representation of Topic instances'''
+        topic = models.Topic.objects.create(
+            name='Sport',
+            link='Test'
+            )
+
+        self.assertEqual(str(topic), topic.name)
